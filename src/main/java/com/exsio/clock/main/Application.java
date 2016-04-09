e50814e56728072ee6324adf67ae0d11c5c1d4f3
eXsio <exsio@gmail.com>
2016-04-09 09:43:47 +0200

    set system look and feel on UI

--- src/main/java/com/exsio/clock/main/Application.java
+++ src/main/java/com/exsio/clock/main/Application.java
@@ -4,18 +4,24 @@ import com.exsio.clock.ui.UI;
 import com.exsio.clock.ui.loading.Loading;
 import com.exsio.clock.util.SpringProfile;
 import com.google.common.collect.Lists;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
+import javax.swing.*;
 import java.util.List;
 
 @SpringBootApplication
 @ComponentScan("com.exsio")
 public class Application extends SpringBootServletInitializer {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
+        setSystemLookAndFeel();
         Loading.show();
         List<String> profiles = getProfiles();
         new SpringApplicationBuilder(Application.class)
@@ -23,6 +29,17 @@ public class Application extends SpringBootServletInitializer {
                 .headless(false)
                 .web(true)
                 .run(args);
+    }
+
+    private static void setSystemLookAndFeel() {
+        if(UI.isAvailable()) {
+            try {
+                UIManager.setLookAndFeel(
+                        UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception e) {
+                LOGGER.error("{}: {}", e.getClass().getName(), e.getMessage(), e);
+            }
+        }
     }
 
     private static List<String> getProfiles() {
